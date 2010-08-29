5a31757511908ca6f727ecdcc34aa5f82cd6419a
Mike Perham <mperham@gmail.com>
2010-08-28 23:22:34 -0700

    stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @now = Rails.cache.fetch('time.now', :expires_in => 30.seconds) { Time.now }
+    @stats = Rails.cache.instance_variable_get(:@data).stats
     @users = Rails.cache.fetch('users.all', :expires_in => 5.seconds) { User.all }
   end
 end
