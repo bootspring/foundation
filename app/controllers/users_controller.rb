080254abc73c7f50194b6105470d4fa40cad1581
Mike Perham <mperham@gmail.com>
2010-08-28 23:18:00 -0700

    Heroku test changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @now = Rails.cache.fetch('time.now', :expires_in => 30.seconds) { Time.now }
     @users = Rails.cache.fetch('users.all', :expires_in => 5.seconds) { User.all }
   end
 end
